600d9e349219c543c46375c1b5905a14eac14394
hytham defrawy <hythamdefrawy@gmail.com>
2022-01-10 23:34:57 -0500

    password validation spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,6 +2,11 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   it 'is valid with valid attribute' do
-    @user = User.new(first)
+    @user = User.new(first_name: "name", email: "email@email.com", password: "password", password_confirmation: "password")
+    expext(@user).to be_valid
+  end
+
+  it 'is not valid without password matches with confirmation password' do
+    @user = User.new(first_name: "name", email: "email@email.com", password: "password", password_confirmation: "pass_word")
   end
 end
